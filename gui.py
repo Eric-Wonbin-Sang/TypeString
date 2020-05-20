17a2f0e899a94d5d36c190d30e5d742753ead9a5
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-05-20 12:04:47 -0400

    Replaced the code in gui with the code in new_gui and deleted new_gui. Also created an index variable to keep track of which word in the type_input_list the user is on.

--- gui.py
+++ gui.py
@@ -2,7 +2,8 @@ import pygame
 import random
 
 import Word
-from General import EasyMode, Constants
+import TypeInput
+from General import EasyMode
 
 
 def do_input_analysis(curr_word, curr_input):
@@ -14,44 +15,42 @@ def do_input_analysis(curr_word, curr_input):
         print("incorrect")
 
 
-def gui():
+def get_left_hand_word_list(word_list, do_shuffle=True):
+    left_hand_word_list = [word for word in Word.order_word_list_by_hand("left", word_list) if
+                           word.left_hand_key_percent == 100]
+    if do_shuffle:
+        random.shuffle(left_hand_word_list)
+    return left_hand_word_list
 
-    # display_height = 1200
-    # display_width = 700
+
+def get_right_hand_word_list(word_list, do_shuffle=True):
+    right_hand_word_list = [word for word in Word.order_word_list_by_hand("left", word_list) if
+                            word.right_hand_key_percent == 100]
+    if do_shuffle:
+        random.shuffle(right_hand_word_list)
+    return right_hand_word_list
+
+
+def gui():
 
     pygame.init()
     pygame.font.init()
-    screen = pygame.display.set_mode([Constants.display_height, Constants.display_width], pygame.RESIZABLE)
-    display_height, display_width = pygame.display.get_surface().get_size()
+    display_height = 1200
+    display_width = 700
+    screen = pygame.display.set_mode([display_height, display_width], pygame.RESIZABLE)
 
     word_list = Word.get_word_list()
-    left_hand_word_list = [word for word in Word.order_word_list_by_hand("left", word_list) if
-                           word.left_hand_key_percent == 100]
-    right_hand_word_list = [word for word in Word.order_word_list_by_hand("left", word_list) if
-                            word.right_hand_key_percent == 100]
-    print(len(left_hand_word_list))
-    print(len(right_hand_word_list))
-
-    random.shuffle(left_hand_word_list)
-    random.shuffle(right_hand_word_list)
+    left_hand_word_list = get_left_hand_word_list(word_list)
+    right_hand_word_list = get_right_hand_word_list(word_list)
 
     curr_word_list = left_hand_word_list
-    # curr_word_list = right_hand_word_list
-    word_index = 0
 
-    input_box = EasyMode.EasyRect(
-        x=display_height/2,
-        y=display_width/2,
-        width=display_height/3,
-        height=display_width/10,
-        color=(66, 135, 245),
-        draw_center=True
-    )
-    obj_list = [input_box]
-
-    word_text_list = [
-        EasyMode.EasyText(
-            text=curr_word_list[word_index].text,
+    type_input_list_index = 0
+    type_input_list = [
+        TypeInput.TypeInput(
+            target_word=next_word.text,
+            display_width=display_width,
+            display_height=display_height,
             x=display_height / 2,
             y=display_width / 2,
             size=display_width / 20,
@@ -60,42 +59,21 @@ def gui():
             opacity=20,
             draw_center=False
         )
+        for i, next_word in enumerate(curr_word_list)
     ]
 
-    next_word_text_list = [
-        EasyMode.EasyText(
-            text=next_word.text,
-            x=display_height / 2,
-            y=display_width / 2 + 50 * i,
-            size=display_width / 20,
-            font_file="FontFolder/Product Sans Regular.ttf",
-            color=(9, 44, 99),
-            opacity=20,
-            draw_center=False
-        )
-        for i, next_word in enumerate(curr_word_list[word_index + 1: + word_index + 4])
-    ]
-    for next_word_text in next_word_text_list:
-        next_word_text.y += 50
+    type_input_y_offset = display_height / 8
+    for i, type_input in enumerate(type_input_list):
+        type_input.move(y_offset=type_input_y_offset * i)
 
-    input_text_list = [
-        EasyMode.EasyText(
-            text="",
-            x=display_height / 2,
-            y=display_width / 2,
-            size=display_width / 20,
-            font_file="FontFolder/Product Sans Regular.ttf",
-            color=(227, 52, 108),
-            draw_center=False,
-            draw_from_bottom=True
-        )
-    ]
-
-    print("{}: ".format(curr_word_list[word_index].text, end=""))
     while True:
 
+        type_input_y_offset = display_height / 8
+
         screen.fill((255, 255, 255))
-        Constants.display_height, Constants.display_width = pygame.display.get_surface().get_size()
+        display_width, display_height = pygame.display.get_surface().get_size()
+
+        print(display_width, display_height, type_input_y_offset)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -103,79 +81,21 @@ def gui():
             if event.type == pygame.VIDEORESIZE:
                 screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
             if event.type == pygame.KEYDOWN:
+
                 if event.key == pygame.K_SPACE:
-                    do_input_analysis(curr_word_list[word_index], input_text_list[0].text)
-
-                    word_index += 1
-                    print("{}: ".format(curr_word_list[word_index].text), end="")
-                    word_text_list[0].text = curr_word_list[word_index].text
-
-                    input_text_list = [
-                        EasyMode.EasyText(
-                            text="",
-                            x=display_height / 2,
-                            y=display_width / 2,
-                            size=display_width / 20,
-                            font_file="FontFolder/Product Sans Regular.ttf",
-                            color=(227, 52, 108),
-                            draw_center=False,
-                            draw_from_bottom=True
-                        )
-                    ] + input_text_list
-
-                    next_word_text_list = [
-                        EasyMode.EasyText(
-                            text=next_word.text,
-                            x=display_height / 2,
-                            y=display_width / 2 + 50 * i,
-                            size=display_width / 20,
-                            font_file="FontFolder/Product Sans Regular.ttf",
-                            color=(9, 44, 99),
-                            opacity=20,
-                            draw_center=False
-                        )
-                        for i, next_word in enumerate(curr_word_list[word_index + 1: + word_index + 4])
-                    ]
-
-                    for next_word_text in next_word_text_list:
-                        next_word_text.y += 50
-
-                    for input_text in input_text_list[1:]:
-                        input_text.y -= 50
-                    input_text_list = input_text_list[:6]
-                    break
-
-                if event.key == pygame.K_BACKSPACE:
-                    if input_text_list[0].text != "":
-                        print(str(chr(event.key)), end="")
-                    input_text_list[0].text = input_text_list[0].text[:-1]
+                    for type_input in type_input_list:
+                        type_input.move(y_offset=-type_input_y_offset)
+                    type_input_list_index += 1
                 else:
-                    if chr(event.key).lower() in "qwertyuiopasdfghjklzxcvbnm'":
-                        print(str(chr(event.key)), end="")
-                        input_text_list[0].text += str(chr(event.key))
-                input_text_list[0].text = input_text_list[0].text
+                    type_input_list[type_input_list_index].update(event=event)
 
-                print([x.text for x in input_text_list])
+        for type_input in type_input_list:
+            type_input.draw(screen=screen)
+            type_input.display_width = display_width
+            type_input.display_height = display_height
 
-        if input_text_list[0].text == curr_word_list[word_index].text:
-            input_text_list[0].color = (28, 252, 3)
-        else:
-            input_text_list[0].color = (252, 3, 3)
-
-        if word_index == len(curr_word_list) - 1:
-            word_index = 0
-
-        for obj in obj_list:
-            obj.update_coordinates(display_height / 2, display_width / 2)
-        for obj in obj_list:
-            obj.draw(screen)
-        for next_word_text in next_word_text_list:
-            next_word_text.draw(screen)
-        for word_text in word_text_list:
-            word_text.draw(screen)
-        for input_text in input_text_list:
-            input_text.draw(screen)
-
+        if type_input_list_index >= len(type_input_list):
+            type_input_list_index = 0
         pygame.display.flip()
 
 
